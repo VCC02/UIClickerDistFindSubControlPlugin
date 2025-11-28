583de76b270669a6009f9639ac38dacd98cfe87a
VCC02 <28475177+VCC02@users.noreply.github.com>
2025-11-28 20:43:37 +0000

    Added a new tool, DistVisor, used for now at sending plugins to UIClicker.

---
+++ DistVisor/DistPluginSender.pas
@@ -0,0 +1,237 @@
+{
+    Copyright (C) 2025 VCC
+    creation date: 28 Nov 2025
+    initial release date: 28 Nov 2025
+
+    author: VCC
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+}
+
+
+unit DistPluginSender;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, ClickerUtils;
+
+
+procedure SendAllDistPlugins(AUIClickerDistBitness: string = '32');  //expected 32 or 64
+procedure SetKeys;
+
+
+implementation
+
+
+uses
+  Process, AsyncProcess;
+
+
+const
+  CClientUnderTestServerPort = '5444'; //The UIClicker, where DistFindSubControl is used.  //see doc
+
+
+function CreateSenderProcess(AExe, AParams, AExeInput: string; ATimeout: Integer = 8000): string;
+var
+  tk: QWord;
+  TempStringList: TStringList;
+  Proc: TAsyncProcess;
+begin
+  Result := '';
+  if not FileExists(AExe) then
+    raise Exception.Create('The executable file must exist at this path: "' + AExe + '"');
+
+  Proc := TAsyncProcess.Create(nil);
+  try
+    Proc.Executable := AExe;
+    Proc.Parameters.Text := FastReplace_45ToReturn(AParams);
+    Proc.CurrentDirectory := ExtractFileDir(AExe);
+    Proc.InheritHandles := True;
+    Proc.Options := [poUsePipes, poStderrToOutPut{, poNewConsole, poNoConsole}];
+
+    Proc.Execute;
+
+    TempStringList := TStringList.Create;
+    try
+      TempStringList.LineBreak := #13#10;    //TBD on Lin
+      TempStringList.Text := FastReplace_45ToReturn(AExeInput);
+      TempStringList.SaveToStream(Proc.Input);
+    finally
+      TempStringList.Free;
+    end;
+
+    tk := GetTickCount64;
+    repeat
+      Sleep(10);
+
+      if GetTickCount64 - tk >= ATimeout then  //WaitForApp until timeout
+      begin
+        WriteLn('Timeout waiting for process to finish: ' + AExe);
+        Break;
+      end;
+    until not Proc.Active;
+
+    if Assigned(Proc.Output) and (Proc.Output <> nil) and (Proc.Output.NumBytesAvailable > 0) then     //read once more (in case the timeout stopped the reading)
+    begin
+      Proc.Output.Position := 0;
+      SetLength(Result, Proc.Output.NumBytesAvailable);
+      Proc.Output.Read(Result[1], Length(Result));
+    end;
+  finally
+    Proc.Free;
+  end;
+end;
+
+
+function SendPlugin(ASenderApp, APluginToBeSent, APluginToBeSentDir, ATXKey, ATXIV, ANextKey, ANextIV: string; ADecryptionPluginName: string = ''): string;
+var
+  DistPath: string;
+  PathToApp, AppParams, AppStdIn: string;
+begin
+  DistPath := ExtractFilePath(ParamStr(0)) + '..\';
+  if (APluginToBeSentDir > '') and (APluginToBeSentDir[Length(APluginToBeSentDir)] <> '\') then
+    APluginToBeSentDir := APluginToBeSentDir + '\';
+
+  PathToApp := DistPath + ASenderApp + '\' + ASenderApp + '.exe';
+  AppParams := '--ClickerClient' + #4#5 + DistPath + '..\UIClicker\ClickerClient\ClickerClient.dll' + #4#5 +
+               '--PluginToBeSent' + #4#5 + DistPath + APluginToBeSentDir + 'lib\' + APluginToBeSent + '.dll' + #4#5 +
+               '--PluginToBeSentDestName' + #4#5 + APluginToBeSent + '.dll' + #4#5 +
+               '--UIClickerAddress' + #4#5 + '127.0.0.1' + #4#5 +
+               '--UIClickerPort' + #4#5 + CClientUnderTestServerPort;
+
+  if ADecryptionPluginName <> '' then
+    AppParams := AppParams + #4#5 + '--DecryptionPluginName' + #4#5 + ADecryptionPluginName + '.dllarc|Mem:\' + ADecryptionPluginName + '.dll';
+
+  AppStdIn := ATXKey + #4#5 +      //This is provided when DistInitialEnc.exe prints "Trasmission Key" on StdOut.
+              ATXIV + #4#5 +       //This is provided when DistInitialEnc.exe prints "Trasmission IV" on StdOut.
+              ANextKey + #4#5 +    //This is provided when DistInitialEnc.exe prints "Subsequent Key" on StdOut.
+              ANextIV + #4#5;      //This is provided when DistInitialEnc.exe prints "Subsequent IV" on StdOut.
+
+  try
+    Result := CreateSenderProcess(PathToApp, AppParams, AppStdIn);
+  except
+    on E: Exception do
+      Result := 'Ex on starting sender process: ' + E.Message;
+  end;
+
+  Result := FastReplace_ReturnTo45(Result);
+end;
+
+
+var
+  InitialTransmissionKey: string = 'dummy_key';
+  InitialTrasmissionIV: string = 'dummy_iv';
+  InitialSubsequentKey: string = 'ABCDEF';
+  InitialSubsequentIV: string = 'IV.IV';
+  DistDecSubsequentKey: string = 'ABCDEFDistDec';
+  DistDecSubsequentIV: string = 'IVDistDec.IV';
+  FindSubControlKey: string = 'ABCDEFFindSubControl';
+  FindSubControlIV: string = 'IVFindSubControl';
+
+function Send_DistInitialDecDll_Via_DistInitialEnc(ADistBitness: string): string;
+begin
+  Result := SendPlugin('DistInitialEnc', ADistBitness + '\DistInitialDec', 'DistInitialDec', InitialTransmissionKey, InitialTrasmissionIV, InitialSubsequentKey, InitialSubsequentIV);
+end;
+
+
+function Send_DistDecDll_Via_DistInitialEnc(ADistBitness: string): string;
+begin
+  Result := SendPlugin('DistInitialEnc', ADistBitness + '\DistDec', 'DistDec', InitialSubsequentKey, InitialSubsequentIV, DistDecSubsequentKey, DistDecSubsequentIV);
+end;
+
+
+function Send_UIClickerDistFindSubControlDll_Via_DistEnc(ADistBitness: string): string;
+begin
+  Result := SendPlugin('DistEnc', ADistBitness + '\UIClickerDistFindSubControl', '', DistDecSubsequentKey, DistDecSubsequentIV, FindSubControlKey, FindSubControlIV, 'DistDec');
+end;
+
+
+function Send_PoolClientDll_Via_DistEnc(ADistBitness: string): string;
+begin
+  Result := SendPlugin('DistEnc', ADistBitness + '\PoolClient', 'PoolClient', DistDecSubsequentKey, DistDecSubsequentIV, FindSubControlKey, FindSubControlIV, 'DistDec');
+end;
+
+
+procedure SendAllDistPlugins(AUIClickerDistBitness: string = '32');  //expected 32 or 64
+const
+  CPrefix = 'Trasmission Key=Trasmission IV=Subsequent Key=Subsequent IV=';
+  COperation = 'Sending plugin to http://127.0.0.1:' + CClientUnderTestServerPort + '/';
+  CExpectedResponse_Unencrypted = CPrefix + 'Sending unencrypted...' + COperation + 'Response: OK';
+  CExpectedResponse_Encrypted = CPrefix + COperation + 'Using archive encryption..Response: OK';
+var
+  DistBitness, Res: string;   //plugin dir bitness
+begin
+  if AUIClickerDistBitness = '32' then
+    DistBitness := 'i386-win32'
+  else
+    if AUIClickerDistBitness = '32' then
+      DistBitness := 'x86_64-win64'
+    else
+      raise Exception.Create('Invalid bitness: "' + AUIClickerDistBitness + '".');
+
+  WriteLn('Sending DistInitialDecDll...');
+  Res := Send_DistInitialDecDll_Via_DistInitialEnc(DistBitness);
+  if Res <> CExpectedResponse_Unencrypted then
+    raise Exception.Create('Error sending DistInitialDecDll: ' + Res);
+  WriteLn('DistInitialDecDll sent.');
+  WriteLn;
+
+  WriteLn('Sending DistDecDll...');
+  Res := Send_DistDecDll_Via_DistInitialEnc(DistBitness);
+  if Res <> CExpectedResponse_Encrypted then
+    raise Exception.Create('Error sending DistDecDll: ' + Res);
+  WriteLn('DistDecDll sent.');
+  WriteLn;
+
+  WriteLn('Sending UIClickerDistFindSubControlDll...');
+  Res := Send_UIClickerDistFindSubControlDll_Via_DistEnc(DistBitness);
+  if Res <> CExpectedResponse_Encrypted then
+    raise Exception.Create('Error sending UIClickerDistFindSubControlDll: ' + Res);
+  WriteLn('UIClickerDistFindSubControlDll sent.');
+  WriteLn;
+
+  WriteLn('Sending PoolClientDll...');
+  Res := Send_PoolClientDll_Via_DistEnc(DistBitness);
+  if Res <> CExpectedResponse_Encrypted then
+    raise Exception.Create('Error sending PoolClientDll: ' + Res);
+  WriteLn('PoolClientDll sent.');
+  WriteLn;
+end;
+
+
+procedure SetKeys;
+begin
+  Randomize;
+  InitialTransmissionKey := 'A new key every time ' + IntToHex(GetTickCount64) + IntToStr(Random(MaxInt));
+  Sleep(33);
+
+  Randomize;
+  InitialTrasmissionIV := 'A new IV every time ' + IntToHex(GetTickCount64) + IntToStr(Random(MaxInt));
+  Sleep(33);
+
+  Randomize;
+  InitialSubsequentKey := 'A new subsequent key every time ' + IntToHex(GetTickCount64) + IntToStr(Random(MaxInt));
+  Sleep(33);
+
+  Randomize;
+  InitialSubsequentIV := 'A new subsequent IV every time ' + IntToHex(GetTickCount64) + IntToStr(Random(MaxInt));
+end;
+
+end.
+
